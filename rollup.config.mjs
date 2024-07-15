e2f70503ad693b1895f652a3ac95b754f6c95147
Andres Vaquero <web.andresvaquero@gmail.com>
2024-07-16 00:19:37 +1000

    adjusting config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,3 +1,5 @@
-import { getBuild } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
-const { build } = getBuild('messages', 'uiComponent');
+import { getBuild, isSlim } from '@arpadroid/arpadroid/src/rollup/builds/rollup-builds.mjs';
+const { build } = getBuild('messages', 'uiComponent', {
+    external: isSlim() && ['forms', 'lists'],
+});
 export default build;
