91f351ebb1976b908de98e424eab0c4928bd3931
Andres Vaquero <web.andresvaquero@gmail.com>
2026-02-23 22:47:14 +1100

    fixing exports, adding scripts, fixing tsconfig

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,2 +1,4 @@
+import '@arpadroid/module/types';
+
 export { MessageConfigType } from './components/message/message.types';
 export { MessagesConfigType } from './components/messages/messages.types';
